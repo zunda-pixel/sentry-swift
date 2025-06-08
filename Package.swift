3c3de28f85b4515b95572936f2cfc50fc0479b06
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-06-09 00:40:46 +0900

    add Test

--- Package.swift
+++ Package.swift
@@ -4,19 +4,39 @@ import PackageDescription
 
 let package = Package(
   name: "sentry-swift",
+  platforms: [
+    .macOS(.v13),
+    .iOS(.v13),
+    .watchOS(.v6),
+    .tvOS(.v13),
+    .visionOS(.v1),
+    .macCatalyst(.v14),
+  ],
   products: [
     .library(
       name: "Sentry",
       targets: ["Sentry"]
     ),
   ],
+  dependencies: [
+    .package(url: "https://github.com/zunda-pixel/http-client.git", from: "0.3.1"),
+    .package(url: "https://github.com/apple/swift-http-types.git", from: "1.4.0")
+  ],
   targets: [
     .target(
-      name: "Sentry"
+      name: "Sentry",
+      dependencies: [
+        .product(name: "HTTPClient", package: "http-client"),
+        .product(name: "HTTPTypes", package: "swift-http-types"),
+        .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
+      ]
     ),
     .testTarget(
       name: "SentryTests",
-      dependencies: ["Sentry"]
+      dependencies: ["Sentry"],
+      resources: [
+        .process("Resources")
+      ]
     ),
   ]
 )
---
+++ Tests/SentryTests/CodableTests.swift
@@ -0,0 +1,41 @@
+import Foundation
+import Testing
+
+@testable import Sentry
+
+@Suite
+struct CodableTests {
+  @Test
+  func event() async throws {
+    let url = Bundle.module.url(forResource: "Event", withExtension: "json")!
+    let data = try Data(contentsOf: url)
+    let event1 = try JSONDecoder().decode(Event.self, from: data)
+    let encodedData = try JSONEncoder().encode(event1)
+    let event2 = try JSONDecoder().decode(Event.self, from: encodedData)
+    print(event1, event2)
+  }
+  
+  @Test
+  func errorEvent() async throws {
+    let url = Bundle.module.url(forResource: "ErrorEvent", withExtension: "json")!
+    let data = try Data(contentsOf: url)
+    let event1 = try JSONDecoder().decode(ErrorEvent.self, from: data)
+    let encodedData = try JSONEncoder().encode(event1)
+    let event2 = try JSONDecoder().decode(ErrorEvent.self, from: encodedData)
+    print(event1, event2)
+  }
+  
+  @Test
+  func logEvent() async throws {
+    let url = Bundle.module.url(forResource: "LogEvent", withExtension: "json")!
+    let data = try Data(contentsOf: url)
+    let event1 = try JSONDecoder().decode(LogEvent<Empty, Empty>.self, from: data)
+    let encodedData = try JSONEncoder().encode(event1)
+    let event2 = try JSONDecoder().decode(LogEvent<Empty, Empty>.self, from: encodedData)
+    print(event1, event2)
+  }
+}
+
+struct Empty: Codable {
+  
+}
